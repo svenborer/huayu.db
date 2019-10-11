809f832231738d4dbae91e5d5a6e25debaa5abce
Sven <borer.sven@gmail.com>
2019-10-11 23:58:10 +0800

    added requirements.txt and updated import script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # huayu.db
+
+All vocabulary and grammar of the Practical Audio Visual Chinese book series can be found in here. I created it to easily lookup examples and find grammar patterns over all the examples provided in the books.
+
+The vocabulary/grammar of the following books is in the database:
+
+Practical Audio Visual Chinese 1 (not yet in database)\
+Practical Audio Visual Chinese 2\
+Practical Audio Visual Chinese 3\
+Practical Audio Visual Chinese 4 (not yet in database)
+
+More to come.
--- import_relations.py
+++ import_relations.py
@@ -1,35 +1,51 @@
 from app import app, db
-from app.models import asoc_translation_example, asoc_grammar_example
+from app.models import Vocabulary, Translation, TranslationExample, asoc_translation_example, asoc_grammar_example
 import csv
 
-with open('csv/tbl_translation_example_asoc.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-            pass
-        else:
-            translation_id = row[0]
-            example_id = row[1]
-            s = asoc_translation_example.insert().values(translation_id=translation_id, example_id=example_id)
-            db.session.execute(s)
-            line_count += 1
-    db.session.commit()
-    print('Processed {} lines.'.format(line_count))
+Vocabulary.query.delete()
+Translation.query.delete()
+TranslationExample.query.delete()
 
-with open('csv/tbl_grammar_example_asoc.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
+with open('csv/main.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
     for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-            pass
-        else:
-            grammar_id = row[0]
-            example_id = row[1]
-            s = asoc_grammar_example.insert().values(grammar_id=grammar_id, example_id=example_id)
+        v_id = row['vocabulary.id']
+        v_hanzi = row['vocabulary.hanzi']
+        v_chapter_id = row['vocabulary.chapter_id']
+        v_number_in_chapter = row['vocabulary.number_in_chapter']
+        if v_id and v_hanzi and v_chapter_id and v_number_in_chapter:
+            v = Vocabulary(
+                id = v_id,
+                hanzi = v_hanzi,
+                chapter_id = v_chapter_id,
+                number_in_chapter = v_number_in_chapter
+            )
+            db.session.add(v)
+        t_id = row['translation.id']
+        t_vocabulary_id = row['translation.vocabulary_id']
+        t_translation_en = row['translation.translation_en']
+        t_gram_term_id = row['translation.gram_term_id']
+        if t_id and t_vocabulary_id and t_translation_en and t_gram_term_id:
+            t = Translation(
+                id = t_id,
+                vocabulary_id = t_vocabulary_id,
+                translation_en = t_translation_en,
+                gram_term_id = t_gram_term_id
+            )
+            db.session.add(t)
+        a_translation_id = row['asoc_translation_example.translation_id']
+        a_example_id = row['asoc_translation_example.example_id']
+        te_example = row['translationexample.example']
+        if a_example_id and te_example:
+            te = TranslationExample(
+                id = a_example_id,
+                example = te_example
+            )
+            db.session.add(te)
+        if a_translation_id and a_example_id:
+            s = asoc_translation_example.insert().values(
+                translation_id=a_translation_id,
+                example_id=a_example_id)
             db.session.execute(s)
-            line_count += 1
-    db.session.commit()
-    print('Processed {} lines.'.format(line_count))
+
+db.session.commit()
